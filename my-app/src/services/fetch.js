c62fde5bb8bf816bfc7a63fe07e5a3074ff35855
emily <emimorgado1205@gamil.com>
2023-10-30 12:01:24 -0400

    fetch.js

--- my-app/src/services/fetch.js
+++ my-app/src/services/fetch.js
@@ -1,3 +1,15 @@
-export function fetch({ method = 'GET', url, boby = null}){
+export async function fetch({ method = 'GET', url, boby = null}){
+    const options = {
+        body, 
+        method, 
+        headers: {'Content-Type' : 'application/json'},
+    }
+    const response = await window.fetch(url,options)
 
+    if (response.ok){
+
+        const resBody = await response.json()
+        return resBody
+    }
+    return null
 }
